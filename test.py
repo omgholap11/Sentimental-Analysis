753ab405daac20eb404d616a35244db12ff666b7
Om Gholap <omgholap051@gmail.com>
2025-11-30 01:38:09 +0530

    tested model

--- test.py
+++ test.py
@@ -1,1 +1,25 @@
-test = "I have completed NLP"
+import pandas as pd
+import joblib
+
+model = joblib.load("Senti_analysis_logical_reggresor.pkl")
+vectorizer = joblib.load("vectorizer_tfidf.pkl")
+
+
+def predict_emotion(text):
+
+    text_vector = vectorizer.transform([text])
+    
+    prediction = model.predict(text_vector)[0]
+    
+    return prediction
+
+if __name__ == "__main__":
+    text = "Where is that stupid."
+    result = predict_emotion(text)
+    print("Input Text:", text)
+    print("Predicted Emotion Number:", result)
+
+    emotions_numbers = ['sadness', 'anger', 'love', 'surprise', 'fear', 'joy']
+
+    print("Predicted Emotions: " , emotions_numbers[result])
+   
